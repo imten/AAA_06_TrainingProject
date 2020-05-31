117a53ff78863427219bb3a2de948c99b261aa6d
xinting <imxt@vip.qq.com>
2020-05-31 14:13:14 +0800

    实训项目上传

--- src/main/java/com/aaa/sys/domain/Permission.java
+++ src/main/java/com/aaa/sys/domain/Permission.java
@@ -10,11 +10,7 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.experimental.Accessors;
 
-/**
- * <p>
- * 
- * </p>
- */
+
 @Data
 @EqualsAndHashCode(callSuper = false)
 @Accessors(chain = true)
